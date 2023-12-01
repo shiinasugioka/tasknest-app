16b0476b5e8640ffcfa526dcdfd5ff0803e0ebef
annie-tu <annietu18@gmail.com>
2023-11-30 16:58:42 -0800

    minor fixes

--- app/src/main/java/edu/uw/ischool/shiina12/tasknest/TaskActivity.kt
+++ app/src/main/java/edu/uw/ischool/shiina12/tasknest/TaskActivity.kt
@@ -18,6 +18,28 @@ class TaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task)
+
+        // https://developer.android.com/develop/ui/views/components/pickers
+        /*findViewById<EditText>(R.id.editTextTime).setOnClickListener {
+            TimePickerFragment().show(supportFragmentManager, "timePicker")
+        }*/
     }
 }
 
+/*
+class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+        // Use the current time as the default values for the picker.
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+
+        // Create a new instance of TimePickerDialog and return it.
+        return TimePickerDialog(activity, this, hour, minute, DateFormat.is24HourFormat(activity))
+    }
+
+    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+        // Do something with the time the user picks.
+    }
+}*/
