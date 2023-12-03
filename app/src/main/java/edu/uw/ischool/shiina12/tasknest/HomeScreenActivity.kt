447e8baedef3d90f7c60a1466ad033f41ff79a64
Shiina Sugioka <shiina12@uw.edu>
2023-12-03 15:40:53 -0800

    added checkboxes

---
+++ app/src/main/java/edu/uw/ischool/shiina12/tasknest/HomeScreenActivity.kt
@@ -0,0 +1,25 @@
+package edu.uw.ischool.shiina12.tasknest
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homescreen)
+
+//      Identify Elements
+        val nest_dropdown = findViewById<Spinner>(R.id.nest_drop_down)
+
+//      Set Element values
+        val nest_dropdown_items =
+            arrayOf<String?>("Sample Nest 1", "Sample Nest 2", "Sample Nest 3")
+        val arrayAdapter =
+            ArrayAdapter<Any?>(this, R.layout.spinner_dropdown_text, nest_dropdown_items)
+        nest_dropdown.adapter = arrayAdapter
+
+
+    }
+}
