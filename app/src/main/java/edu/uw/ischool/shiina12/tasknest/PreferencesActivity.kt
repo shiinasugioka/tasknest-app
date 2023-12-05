9096ae442b533fbe09d54764d96197a5c763fa1b
Matthew Bacarro <mbacarro@uw.edu>
2023-12-05 15:54:49 -0800

    Added Logging when PreferencesActivity is launched and fixed an import issue

--- app/src/main/java/edu/uw/ischool/shiina12/tasknest/PreferencesActivity.kt
+++ app/src/main/java/edu/uw/ischool/shiina12/tasknest/PreferencesActivity.kt
@@ -1,12 +1,19 @@
 package edu.uw.ischool.shiina12.tasknest
+
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 
 class PreferencesActivity : AppCompatActivity() {
 
+    private val TAG = "PreferenceActivity"
+    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(TAG, "Preference Activity Launched")
+        
         supportFragmentManager.beginTransaction()
             .replace(android.R.id.content, SettingsFragment())
             .commit()
