e516da02df3eb8d5339574bdccef5eb5166b843b
Matthew Bacarro <mbacarro@uw.edu>
2023-12-05 16:52:00 -0800

    added menu popup for app and nest settings

--- app/src/main/java/edu/uw/ischool/shiina12/tasknest/HomeScreenActivity.kt
+++ app/src/main/java/edu/uw/ischool/shiina12/tasknest/HomeScreenActivity.kt
@@ -1,7 +1,11 @@
 package edu.uw.ischool.shiina12.tasknest
 
 import android.os.Bundle
+import android.view.ContextThemeWrapper
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
@@ -20,6 +24,48 @@ class HomeScreenActivity : AppCompatActivity() {
             ArrayAdapter<Any?>(this, R.layout.spinner_dropdown_text, nest_dropdown_items)
         nest_dropdown.adapter = arrayAdapter
 
+        // Popup settings
+        val settings_button = findViewById<ImageButton>(R.id.settings_button)
+        settings_button.setOnClickListener{
+            showPopupMenu(it)
+        }
+
 
     }
+
+    private fun showPopupMenu(view: View) {
+        val contextWrapper = ContextThemeWrapper(this, R.style.PopupMenuStyle)
+        val popupMenu = PopupMenu(contextWrapper, view)
+        popupMenu.inflate(R.menu.settings_popup_menu)
+
+        popupMenu.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                // Group 1: Nest Settings
+                R.id.group_nest_settings, R.id.menu_delete_nest -> {
+                    // Handle Delete Nest or other actions in this group
+                    true
+                }
+                R.id.menu_rename_nest -> {
+                    // Handle Rename Nest action
+                    true
+                }
+
+                // Group 2: App Settings
+                R.id.group_app_settings, R.id.menu_settings -> {
+                    // Handle Settings or other actions in this group
+                    true
+                }
+                R.id.menu_google -> {
+                    // Handle Google action
+                    true
+                }
+                else -> false
+            }
+        }
+
+        popupMenu.show()
+    }
+
+
+
 }
--- app/src/main/java/edu/uw/ischool/shiina12/tasknest/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/shiina12/tasknest/MainActivity.kt
@@ -3,12 +3,17 @@ package edu.uw.ischool.shiina12.tasknest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i(TAG, "MainActivity Launched")
 
         val homeScreenButton: Button = findViewById(R.id.homescreen_button)
 
