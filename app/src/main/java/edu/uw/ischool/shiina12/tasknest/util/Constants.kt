fe3e27ddc6b6886c344272df792207f4dfc37193
Shiina Sugioka <shiina12@uw.edu>
2023-12-07 15:15:15 -0800

    created constants

---
+++ app/src/main/java/edu/uw/ischool/shiina12/tasknest/util/Constants.kt
@@ -0,0 +1,9 @@
+package edu.uw.ischool.shiina12.tasknest.util
+
+object Constants {
+    const val REQUEST_ACCOUNT_PICKER = 1000
+    const val REQUEST_AUTHORIZATION = 1001
+    const val REQUEST_GOOGLE_PLAY_SERVICES = 1002
+    const val PREF_ACCOUNT_NAME = "tasknest_app"
+    const val APPLICATION_NAME = "TaskNest App"
+}
