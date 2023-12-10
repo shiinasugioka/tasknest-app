5852b7b521b69e26918603645d5f17259c57f076
Shiina Sugioka <shiina12@uw.edu>
2023-12-09 20:28:54 -0800

    removed dead code

--- app/src/main/java/edu/uw/ischool/shiina12/tasknest/TaskApp.kt
+++ app/src/main/java/edu/uw/ischool/shiina12/tasknest/TaskApp.kt
@@ -5,14 +5,11 @@ import android.util.Log
 
 class TaskApp : Application() {
     private val TAG : String = "TaskApp"
-    /*val todoRepository: TodoRepository = InMemoryTodoRepository()
-    var todoNests: MutableList<TodoNest> = mutableListOf()*/
 
     override fun onCreate() {
         super.onCreate()
         instance = this
         Log.d(TAG, "TaskApp is being loaded and run.")
-        // todoNests = todoRepository.getNest()
     }
 
     companion object {
